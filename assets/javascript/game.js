a8a0fd73ad214e7e288e40c2ea7e54c1ecb63bcd
EvJCle <evanjcleary@gmail.com>
2019-06-02 22:04:23 -0400

    added html and then started on javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+// We're going to create a game where I will guess 9 times a letter, and if that
+// letter is correct than I win. If It's not correct, I lose.
+
+
+// Create Global Variables here
+var winText = document.getElementById("win-count");
+var lossText = document.getElementById("loss-count");
+var guessCount = document.getElementById("guess-count");
+var guessList = document.getElementById("guess-list");
+
+// 
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+
+
+// Start to create Code here: 
+// First when a key is pressed, the game begins
+
+
+// The computer will automatically put a number in it's reserve
+
+// Then the user can press 8 more times to guess the number if that number === computer number then you win
+
+// Then we're going to make it so that the integers show the increase with each guess
+
+function displayScores() {
+    winText.textContent = wins;
+    lossText.textContent = losses;
+    guessCount.textContent = guesses;
+}
+
+
+displayScores()
