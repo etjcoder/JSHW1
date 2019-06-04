63f67e6c63475fdc33c413e9ae12cf863ad212cd
Evan Desktop <evanjcleary@gmail.com>
2019-06-03 23:54:29 -0400

    made many changes to js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,28 +7,17 @@ var winText = document.getElementById("win-count");
 var lossText = document.getElementById("loss-count");
 var guessCount = document.getElementById("guess-count");
 var guessList = document.getElementById("guess-list");
-
+var computerOptions = ['a', 'e', 'i', 'o', 'u'];
+var resetButton = document.getElementById("reset-button");
 // 
 var wins = 0;
 var losses = 0;
 var guesses = 9;
 
-
-// Start to create Code here: 
-// First when a key is pressed, the game begins
-
-
-function gameStart(e) {
-    var computerChoice = computerGuesser();
-    console.log(computerChoice);
+function computerGuesser() {
+    var computerNumber = computerOptions[Math.floor(Math.random() * 5)];
+    return computerNumber;
 }
-
-document.onkeydown = gameStart();
-// The computer will automatically put a number in it's reserve
-
-// Then the user can press 8 more times to guess the number if that number === computer number then you win
-
-// Then we're going to make it so that the integers show the increase with each guess
 
 function displayScores() {
     winText.textContent = wins;
@@ -36,10 +25,53 @@ function displayScores() {
     guessCount.textContent = guesses;
 }
 
-function computerGuesser() {
-    var computerOptions = ['a','e','i','o','u'];
-    var computerNumber = computerOptions[Math.floor(Math.random() * 5)];
-    return computerNumber;
-}
 
+computerGuess = computerGuesser();
+console.log(computerGuess);
+
+
+
+
+//This is the code that will run the game once the user presses a key
+// document.addEventListener('keydown', logKey);
+
+function logKey() {
+    var x = event.key;
+    guessList.textContent = x;
+    console.log(x);
+    if (x === computerGuess) {
+        wins++;
+    }
+
+    else {
+        guesses--;
+    }
+
+    if (guesses === 0) {
+        losses++;
+    }
+    
+    document.getElementById("win-count").textContent = wins;
+    document.getElementById("loss-count").textContent = losses;
+    document.getElementById("guess-count").textContent = guesses;
+}
+// Game will start on key press
+document.onkeyup = logKey;
+
+//Add event listener to our reset button when user clicks it
+resetButton.addEventListener("click", displayScores);
+
+// Original Score setting as file loads
 displayScores();
+
+
+// function userGuess() {
+//     var key = document.onkeyup;
+//     console.log(key);
+// }
+
+// document.onkeyup = userGuess(); 
+
+
+
+// document.onkeydown = gameStart();
